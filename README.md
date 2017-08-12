8d363aa1332ff1aecd281dfdabbc2e0a654cd607
Darren Hickling <darren@hickling.is>
2017-08-12 13:26:43 +0100

    Finally! Adding my setup to Git

---
+++ README.md
@@ -0,0 +1,9 @@
+# Setup
+## Introduction
+Just a group of scripts that set up my common environments.
+
+These can be re-run at any time, and will update all pre-installed apps.
+
+## Running
+### Windows
+From an Administrator PowerShell terminal, just run `.\setup.ps1`.
---
+++ setup.ps1
@@ -0,0 +1,51 @@
+function Write-Heading ($text)
+{
+    Write-Output ""
+    Write-Host -ForegroundColor green $text
+}
+
+function Write-Title ($text)
+{
+    Write-Output ""
+    Write-Host -BackgroundColor yellow -ForegroundColor blue $text
+}
+
+Write-Title "Setup"
+Write-Heading "Configuring Chocolatey"
+
+@("allowEmptyChecksumsSecure", "allowGlobalConfirmation") `
+    | ForEach-Object { choco feature enable --name $_ }
+
+Install-Module PSReadline -Confirm
+$chocoConfig = Get-Content -Raw -Path .\chocolatey-apps.json | ConvertFrom-Json
+
+function Install-FromChocolatey ($key)
+{
+    Write-Heading "Installing $key from Chocolatey"
+    $chocoConfig.$key | ForEach-Object { cinst $_ }
+}
+
+Install-FromChocolatey "Tools"
+Install-FromChocolatey "Languages"
+
+Write-Heading "Configuring Git"
+git config --global core.autocrlf true
+git config --global credential.helper wincred
+
+Write-Heading "Language extensions"
+refreshenv
+gem install jekyll bundler --no-document
+npm install gulp -g
+pip install flask
+pip install pygments
+
+Write-Heading "Configuring Windows"
+Install-WindowsFeature -Name Web-Asp-Net
+
+Install-FromChocolatey "Applications"
+
+Write-Heading "Updating existing Chocolatey apps"
+choco upgrade all
+
+Write-Title "Done!"
+Write-Output ""
