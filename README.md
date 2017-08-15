7b4efa82f4bc01ef55755e5547ae82099e231eae
Darren Hickling <darren@hickling.is>
2017-08-15 21:23:23 +0100

    Adding Fedora setup

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 ## Introduction
 Just a group of scripts that set up my common environments.
 
-These can be re-run at any time, and will update all pre-installed apps.
+These can be re-run at any time, and will update pre-installed packages, runtimes and their associated packages, such as Ruby gems.
 
 ## Running
+### Fedora
+Just run `sudo sh setup.sh` from a terminal. This should be simple enough to adapt for another distro, e.g. Ubuntu.
+
 ### Windows
-From an Administrator PowerShell terminal, just run `.\setup.ps1`.
+From an Administrator PowerShell terminal, run `.\setup.ps1`.
---
+++ setup.sh
@@ -0,0 +1,40 @@
+BLUE="\033[0;34m"
+NC="\033[0m" # No Colour
+YELLOW="\033[1;33m"
+
+echo -e "${BLUE}Running Setup${NC}"
+
+echo -e "${YELLOW}Registering new repositories...${NC}"
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+echo -e "${YELLOW}Updating the package cache...${NC}"
+dnf check-update
+
+echo -e "${YELLOW}Installing applications...${NC}"
+dnf install \
+    chromium \
+    code \
+    git \
+    nodejs \
+    openssl-devel \
+    ruby \
+    yum-utils
+
+echo -e "${YELLOW}Updating applications...${NC}"
+# Confirm interactively, rather than auto-accept!
+dnf update
+
+echo -e "${YELLOW}Installing runtime packages...${NC}"
+gem install jekyll bundler --no-document
+npm install gulp -g
+pip install flask pygments --user
+
+echo -e "${YELLOW}Updating runtime packages...${NC}"
+gem update --no-document
+npm update -g
+
+echo -e "${YELLOW}Cleaning up...${NC}"
+dnf clean all
+
+echo -e "${BLUE}Done!${NC}"
