0683ad0343205cfebef2e4f8f1a92593b4dc87e6
Darren Hickling <darrenhickling@hotmail.com>
2019-10-29 22:52:41 +0000

    Adding support for awesome Windows Terminals

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # Setup
-## Introduction
+
+## 👋 Introduction
+
 Just a group of scripts that set up my common environments.
 
-These can be re-run at any time, and will update pre-installed packages, runtimes and their associated packages, such as Ruby gems.
+These can be re-run at any time, and will update pre-installed packages, runtimes and their associated packages, and global configuration.
 
-## Running
+## 🏃‍ Running
+
 ### Fedora
-Just run `sudo sh setup.sh` from a terminal. This should be simple enough to adapt for another distro, e.g. Ubuntu.
+
+Run `sudo sh setup.sh` from a terminal. This should be simple enough to adapt for another distro, e.g. Ubuntu.
 
 ### Windows
+
 From an Administrator PowerShell terminal, run `.\setup.ps1`.
+
+## 🤦‍ Limitations
+
+Official Windows Store apps cannot be installed from the command line. As such, I recommend installing Windows Terminal (Preview) manually.
+
+## 💡 Inspiration
+
+- [Scott Hanselman]:  How to make a pretty prompt in Windows Terminal with Powerline, Nerd Fonts, Cascadia Code, WSL, and oh-my-posh
+
+[Scott Hanselman]: https://www.hanselman.com/blog/HowToMakeAPrettyPromptInWindowsTerminalWithPowerlineNerdFontsCascadiaCodeWSLAndOhmyposh.aspx
--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,9 @@
-function Write-Heading ($text)
-{
+function Write-Heading ($text) {
     Write-Output ""
     Write-Host -ForegroundColor green $text
 }
 
-function Write-Title ($text)
-{
+function Write-Title ($text) {
     Write-Output ""
     Write-Host -BackgroundColor yellow -ForegroundColor blue $text
 }
@@ -14,15 +12,11 @@ Write-Title "Setup"
 Write-Heading "Configuring Chocolatey"
 
 @("allowEmptyChecksumsSecure", "allowGlobalConfirmation") `
-    | ForEach-Object { choco feature enable --name $_ }
-
-Install-Module PSReadline -Confirm
-Install-Module z -Confirm
+| ForEach-Object { choco feature enable --name $_ }
 
 $chocoConfig = Get-Content -Raw -Path .\chocolatey-apps.json | ConvertFrom-Json
 
-function Install-FromChocolatey ($key)
-{
+function Install-FromChocolatey ($key) {
     Write-Heading "Installing $key from Chocolatey"
     $chocoConfig.$key | ForEach-Object { cinst $_ }
 }
@@ -34,19 +28,31 @@ Write-Heading "Configuring Git"
 git config --global core.autocrlf true
 git config --global credential.helper wincred
 
-Write-Heading "Language extensions"
-refreshenv
-gem install jekyll bundler --no-document
-python -m pip install --upgrade pip
-pip install flask pygments
+Write-Heading "Installing Modules"
+Install-Module oh-my-posh -Confirm
+Install-Module posh-git -Confirm
+Install-Module PSReadline -Confirm
+Install-Module z -Confirm
 
-Write-Heading "Configuring Windows"
-Install-WindowsFeature -Name Web-Asp-Net
+Write-Heading "Installing modules for languages"
+refreshenv
+gem install bundler wdm --no-document
+python -m pip install --upgrade pip
 
 Install-FromChocolatey "Applications"
 
 Write-Heading "Updating existing Chocolatey apps"
 choco upgrade all
+
+Write-Heading "Copying PowerShell Profile"
+Copy-Item powershell-profile.ps1 $PROFILE
+
+Write-Heading "Installing Fonts"
+iwr -useb get.scoop.sh | iex
+scoop install aria2
+scoop bucket add nerd-fonts
+scoop install sudo
+sudo scoop install Delugia-Code
 
 Write-Title "Done!"
 Write-Output ""
