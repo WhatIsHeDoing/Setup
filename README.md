dc3b5d9066f994ad61e7a1d7c82b61e553d79b47
Darren Hickling <darrenhickling@hotmail.com>
2023-10-01 19:05:42 +0100

    ➕ Adding Docker - Using new Ubuntu recording - Installing global VS Code extensions

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 ## 👋 Introduction
 
-Just a group of scripts that set up my common environments.
+This repository contains a group of scripts that set up my common environments.
 
 These can be re-run at any time, and will update pre-installed packages, runtimes and their associated packages, plus global configuration.
+Each script is designed to set up each Operating System (OS) using similar tools where possible,
+such as [Starship] as the shell prompt and [VS Code] as the code editor,
+to simplify moving between them.
 
 Click on the Ubuntu version below to see it in action:
 
-[![asciicast](https://asciinema.org/a/577454.svg)](https://asciinema.org/a/577454?autoplay=1)
+[![asciicast](https://asciinema.org/a/611447.svg)](https://asciinema.org/a/611447?autoplay=1)
 
 ## 🏃‍ Running
 
@@ -26,3 +29,5 @@ sudo sh setup_ubuntu.sh
 - [Scott Hanselman]: How to make a pretty prompt in Windows Terminal with Powerline, Nerd Fonts, Cascadia Code, WSL, and oh-my-posh
 
 [Scott Hanselman]: https://www.hanselman.com/blog/HowToMakeAPrettyPromptInWindowsTerminalWithPowerlineNerdFontsCascadiaCodeWSLAndOhmyposh.aspx
+[Starship]: https://starship.rs/
+[VS Code]: https://code.visualstudio.com/
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -15,16 +15,10 @@ echo "                   /_/      "
 echo ""
 printf "${BLUE}Running Setup...${NC}\n"
 
-printf "${YELLOW}Adding package repositories...${NC}\n"
-apt-add-repository ppa:zanchey/asciinema -y
-
 echo ""
-printf "${YELLOW}Updating the package cache...${NC}\n"
+printf "${YELLOW}Updating and installing initial packages...${NC}\n"
 apt-get upgrade -y
 apt-get update
-
-echo ""
-printf "${YELLOW}Installing initial packages...${NC}\n"
 
 apt install -y \
     apt-transport-https \
@@ -34,13 +28,27 @@ apt install -y \
 echo ""
 printf "${YELLOW}Registering new package repositories...${NC}\n"
 
-printf "${GREEN}Node.js...${NC}\n"
+printf "${GREEN}asciinema...${NC}\n"
+apt-add-repository ppa:zanchey/asciinema -y
+
 # https://github.com/nodesource/distributions#ubuntu-versions
+printf "${GREEN}Node.js...${NC}\n"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg --yes
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+printf "${GREEN}Docker...${NC}\n"
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg --yes
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list
+
+# https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
 printf "${GREEN}kubetcl...${NC}\n"
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg --yes
 # This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
@@ -53,6 +61,11 @@ apt update
 apt install -y \
     apt-utils \
     asciinema \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
     fzf \
     git \
     gnupg \
@@ -104,6 +117,10 @@ fi
 
 printf "${GREEN}Updating Rust...${NC}\n"
 rustup update
+
+printf "${YELLOW}Installing global VS Code extensions...${NC}\n"
+code --install-extension sdras.night-owl
+code --install-extension vscode-icons-team.vscode-icons
 
 printf "${YELLOW}Cleaning up...${NC}\n"
 apt autoremove -y
